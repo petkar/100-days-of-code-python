c9c2205498fa1b2d031fe23fc5818a47b1253700
petkar <59476886+petkar@users.noreply.github.com>
2022-08-21 23:55:11 +0530

    Number guess game

--- blackjack/main.py
+++ blackjack/main.py
@@ -13,8 +13,7 @@ def clear():
 def deal_card():
     """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
+    return random.choice(cards)
 
 
 def calculate_score(cards):
